ec7a9bca194a37d517f92d8df8681f9a52d318dd
goblegue <hmehboob.bese21seecs@seecs.edu.pk>
2024-06-12 15:38:40 +0500

    integration test done

--- foundry-fund-me/script/interaction.s.sol
+++ foundry-fund-me/script/interaction.s.sol
@@ -1,36 +1,46 @@
-//SPDX-License-Identifier: MIT
+//SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/Fundme.sol";
-import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {DeployFundMe} from "./DeployFundMe.s.sol";
 
 contract FundFundMe is Script {
-    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 SEND_VALUE = 0.1 ether;
 
-    function fundFundMe(address mostRecentlyDeployed) public {
-        
-        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
-        
-        console.log("Funded FundMe with %s",SEND_VALUE);
-    }
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
 
-    function run() public {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
         vm.startBroadcast();
         fundFundMe(mostRecentlyDeployed);
         vm.stopBroadcast();
     }
+
+    function fundFundMe(address mostRecentlyDeployed) public payable {
+        vm.prank(msg.sender);
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
 }
 
 contract WithdrawFundMe is Script {
-    FundMe fundMe;
-
-    function initialize(FundMe _fundMe) public {
-        fundMe = _fundMe;
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        withdrawFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
     }
 
-    function run() public {
-        fundMe.withdraw();
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.prank(msg.sender);
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        console.log("Withdrew funds");
     }
 }
